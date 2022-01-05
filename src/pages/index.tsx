e9db692e0cf29df8b3534606a9bea60f3a41420f
Jennifer Takagi <jennitakagi@gmail.com>
2022-01-05 15:24:15 +0100

    chore: add Prettier to the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -16,11 +16,10 @@ export default function Home() {
         </h1>
 
         <p>
-          Get started by editing{' '}
-          <code >pages/index.js</code>
+          Get started by editing <code>pages/index.js</code>
         </p>
 
-        <div >
+        <div>
           <a href="https://nextjs.org/docs">
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -31,16 +30,12 @@ export default function Home() {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples">
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
+          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
             <h2>Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -62,5 +57,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
